e105df460ec3baf013f831db2c20cb6315552898
bajcyk <94803663+bajcyk@users.noreply.github.com>
2024-03-13 11:36:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function sprawdz() {
+    var a = document.getElementById("a").checked;
+    var b = document.getElementById("b").checked;
+    var c = document.getElementById("c").checked;
+
+    console.log(a, b, c);
+
+    if(a == false && b == false && c == false) {
+        document.getElementById("wynik").innerHTML = "Zaznacz odpowiedź";
+        wynik.style.color = "orange";
+    }
+    else {
+        if(a == true) {
+            document.getElementById("wynik").innerHTML = "TAK";
+            wynik.style.color = "green";
+            $('.klasa').css('background-color: green;');
+        }
+        else {
+            document.getElementById("wynik").innerHTML = "NIE";
+            wynik.style.color = "red";
+        }
+    }
+}
